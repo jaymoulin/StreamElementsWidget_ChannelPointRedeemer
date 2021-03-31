8a92d8d1bdd13d260c2e27c3c5ed8043f8f6f7ff
Jay MOULIN <jaymoulin@gmail.com>
2021-03-31 02:12:19 +0200

    Remove useless redemption id

--- tools.js
+++ tools.js
@@ -3,6 +3,5 @@ module.exports = {
         return new Promise(res => {
             setTimeout(res, milliseconds)
         });
-    },
-    FREE_RANDOM_ALBUM: '655c3a9a-ef36-4203-bc29-2833cbc3759d'
+    }
 }
